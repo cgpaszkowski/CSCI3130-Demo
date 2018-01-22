7bbf65b9a30bd126f7bf327daf05308e260c1319
cgpaszkowski <polishstriker_12@live.ca>
2018-01-21 23:42:20 -0400

    Assignment_1_Final_Commit

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView viewTextBox;
+    EditText inputTextBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputTextBox = (EditText) findViewById(R.id.editText2);
+        viewTextBox = (TextView) findViewById(R.id.textView2);
+        Button submitButton = (Button) findViewById(R.id.Submit_Button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String randomText = inputTextBox.getText().toString();
+                viewTextBox.append(randomText);
+            }
+        });
     }
 }
